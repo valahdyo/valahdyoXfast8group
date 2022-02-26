4cf7ebd23a237bd024927e2446d765199cfa0157
Valahdyo Arbandy <varbandy@gmail.com>
2022-02-26 14:10:37 +0700

    install cors

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express")
-const app = express()
 const router = require("./src/routes")
+const cors = require("cors")
+
+const app = express()
+require("dotenv").config()
+
 app.use(express.json())
+app.use(cors())
 
 //base url {url}/api
 app.use("/api", router)
+app.use("/uploads", express.static("uploads"))
 
 port = process.env.PORT || 5000
 app.listen(port, () => {

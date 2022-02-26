41f65e5983ce69b9cc9fec9b14870af0e5a776bc
Valahdyo Arbandy <varbandy@gmail.com>
2022-02-26 19:44:05 +0700

    add dummy product

--- seeders/20220122151816-demo-products.js
+++ seeders/20220122151816-demo-products.js
@@ -1,6 +1,5 @@
 "use strict"
-// createdAt: new Date(),
-// updatedAt: new Date(),
+
 module.exports = {
   up: async (queryInterface, Sequelize) =>
     await queryInterface.bulkInsert(
@@ -14,6 +13,8 @@ module.exports = {
           sellPrice: 1319400,
           description:
             "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "Mens Casual Premium Slim Fit T-Shirts ",
@@ -23,6 +24,8 @@ module.exports = {
           sellPrice: 267600,
           description:
             "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with  round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "Mens Cotton Jacket",
@@ -32,6 +35,8 @@ module.exports = {
           sellPrice: 671880,
           description:
             "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions,  such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "Mens Casual Slim Fit",
@@ -41,6 +46,8 @@ module.exports = {
           sellPrice: 191880,
           description:
             "The color could be slightly different between on the screen and in practice. /  Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title:
@@ -51,6 +58,8 @@ module.exports = {
           sellPrice: 8340000,
           description:
             "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "Solid Gold Petite Micropave ",
@@ -60,6 +69,8 @@ module.exports = {
           sellPrice: 2016000,
           description:
             "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "White Gold Plated Princess",
@@ -69,6 +80,8 @@ module.exports = {
           sellPrice: 119880,
           description:
             "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "Pierced Owl Rose Gold Plated Stainless Steel Double",
@@ -78,6 +91,8 @@ module.exports = {
           sellPrice: 131880,
           description:
             "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
@@ -87,6 +102,8 @@ module.exports = {
           sellPrice: 768000,
           description:
             "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
@@ -96,6 +113,8 @@ module.exports = {
           sellPrice: 1308000,
           description:
             "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host  device, OS and application.)",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title:
@@ -106,6 +125,8 @@ module.exports = {
           sellPrice: 1308000,
           description:
             "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Ero  r Checking & Correction) to provide the optimized performance and enhanced reliability.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title:
@@ -116,6 +137,8 @@ module.exports = {
           sellPrice: 1368000,
           description:
             "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title:
@@ -126,6 +149,8 @@ module.exports = {
           sellPrice: 7188000,
           description:
             "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync  technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178  degree. Vertical viewing angle-178 degree 75 hertz",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title:
@@ -136,6 +161,8 @@ module.exports = {
           sellPrice: 11999880,
           description:
             "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningl   realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
@@ -145,6 +172,8 @@ module.exports = {
           sellPrice: 683880,
           description:
             "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title:
@@ -155,6 +184,8 @@ module.exports = {
           sellPrice: 359400,
           description:
             "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
@@ -164,6 +195,8 @@ module.exports = {
           sellPrice: 479880,
           description:
             "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The  Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and  the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "MBJ Women's Solid Short Sleeve Boat Neck V ",
@@ -173,6 +206,8 @@ module.exports = {
           sellPrice: 118200,
           description:
             "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "Opna Women's Short Sleeve Moisture",
@@ -182,6 +217,8 @@ module.exports = {
           sellPrice: 95400,
           description:
             "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           title: "DANVOUY Womens T Shirt Casual Cotton Short",
@@ -191,6 +228,8 @@ module.exports = {
           sellPrice: 155880,
           description:
             "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
       ],
       {}

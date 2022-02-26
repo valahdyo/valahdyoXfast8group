8186d9335ce0f95a8db3e3f1dee809e328d0b3a3
Valahdyo Arbandy <varbandy@gmail.com>
2022-02-26 20:38:00 +0700

    add readme

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const app = express()
 require("dotenv").config()
 
 app.use(cors({ origin: "*" }))
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
 app.use(express.json())
 
 //base url {url}/api
